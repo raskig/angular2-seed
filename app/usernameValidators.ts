a841d40b3f8c53d1b633275fdc099eeb1795c6c6
Gabor Raski <gabor.raski@gmail.com>
2017-07-07 16:15:01 +0100

    async validator added.

--- app/usernameValidators.ts
+++ app/usernameValidators.ts
@@ -1,6 +1,20 @@
 import {Control} from "angular2/common";
 
 export class UsernameValidators{
+
+    static shouldBeUnique(control: Control){
+        return new Promise((resolve, reject) => {
+            setTimeout(function(){
+                if(control.value=='gabor'){
+                    resolve({shouldBeUnique: true});
+                }
+                else{
+                    resolve(null);
+                }
+            }, 1000);
+        });
+    }
+
     static cannotContainSpace(control: Control){
         if(control.value.indexOf(' ') >= 0){
             return {
