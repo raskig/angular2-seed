06ac4e3ce5ae80191afe58544b0811f0f6942713
Gabor Raski <gabor.raski@gmail.com>
2017-07-07 14:10:53 +0100

    cretae control group with FormBuilder

--- app/signup-form.component.ts
+++ app/signup-form.component.ts
@@ -1,15 +1,24 @@
 import {Component} from 'angular2/core';
-import {Control, ControlGroup, Validators} from "angular2/common";
+import {Control, ControlGroup, FormBuilder, Validators} from "angular2/common";
 
 @Component({
     selector: 'signup-form',
     templateUrl: 'app/signup-form.component.html'
 })
 export class SignUpFormComponent {
-    form = new ControlGroup({
-        username: new Control('', Validators.required),
-        password: new Control('', Validators.required)
-    });
+    // form = new ControlGroup({
+    //     username: new Control('', Validators.required),
+    //     password: new Control('', Validators.required)
+    // });
+
+    form: ControlGroup;
+
+    constructor(fb : FormBuilder){
+        fb.group({
+            username:['', Validators.required],
+            password: ['', Validators.required]
+        })
+    }
 
     signup(){
         console.log(this.form.value);
